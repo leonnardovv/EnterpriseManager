364953719e78f73c51a157ff2ed6b6adffd2102d
Bogdan <bogdancatrinoiu@gmail.com>
2017-08-05 01:59:05 +0300

    fixing all

--- Manager/IntroductionApp/models.py
+++ Manager/IntroductionApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length = 60, unique=True)
+    mobile_number = models.CharField(max_length = 40)
+    user = models.OneToOneField(
+        User,
+        primary_key=True,
+        related_name='profile')
+
+    class Meta:
+        verbose_name_plural = 'User Profiles'
